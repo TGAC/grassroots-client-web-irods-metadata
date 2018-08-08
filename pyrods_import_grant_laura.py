91b18dda4545ab7c4934feb397c71518df073d9f
Xigndong Bian <xingdong.bian@earlham.ac.uk>
2018-08-08 16:42:16 +0100

    add new json list metadata adding method

--- pyrods_import_grant_laura.py
+++ pyrods_import_grant_laura.py
@@ -28,7 +28,6 @@ veryverbose = True
 # 37e03dce-30c8-4e98-b76c-7d0c53d4896d - bernardo
 
 # b0f20e9f-23f6-4cd3-bfd3-3910a1d472e8 - toronto index page
-# 3bc739fe-4fa1-4841-afc5-470f43a9a074 - laura 2
 
 # grassroots-4
 #
@@ -41,7 +40,6 @@ veryverbose = True
 # 5bb7cdc1-ddb0-4354-b74a-51a78f678341 - toronto index page
 
 # 3f00675f-836c-4364-9210-71794d8ff26e - ricardo
-# 9d2dd068-839e-41cb-a9f9-6266338f4212 - laura2
 
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -54,29 +52,47 @@ def is_json(myjson):
         return False
     return True
 
-
+ricardoUUID = "3f00675f-836c-4364-9210-71794d8ff26e"
 wuffprojectUUID = "9791ca43-ddf0-4bfe-8fc0-50ff69cd6229"
-lauraproject2UUID = "9d2dd068-839e-41cb-a9f9-6266338f4212"
+lauraprojectUUID = "ffc3e90f-7f89-4934-8e35-943bb64d1789"
 bernardoprojectUUID = "b72884b3-5e52-4032-82d7-c0a3da40d171"
-projectUUID = lauraproject2UUID
+projectUUID = wuffprojectUUID
+
+g1 = "BB/N005104/1"
+g2 = "BB/N005155/1"
+g3 = "BB/L011786/1"
+g4 = "BB/N020871/1"
+g5 = "BB/P016855/1"
 
 sess = iRODSSession(host='localhost', port=1247, user='rods', password='rods', zone='grassrootsZone')
-coll = sess.collections.get("/grassrootsZone/public/under_license/toronto/Gardiner_2018-")
+coll = sess.collections.get("/grassrootsZone/public/under_license/toronto/Gardiner_2018-01-29_Watkins-diversity-12Mb")
 
-coll.metadata.add("project_uuid", projectUUID)
+coll.metadata.add("Project Code", g1)
+coll.metadata.add("Project Code", g2)
+coll.metadata.add("Project Code", g3)
+coll.metadata.add("Project Code", g4)
+coll.metadata.add("Project Code", g5)
 
 for collection, subcollections, data_objects in coll.walk(topdown=True):
 
     if (len(data_objects) > 0):
         for dobj in data_objects:
 
-            dobj.metadata.add("project_uuid", projectUUID)
+            dobj.metadata.add("Project Code", g1)
+            dobj.metadata.add("Project Code", g2)
+            dobj.metadata.add("Project Code", g3)
+            dobj.metadata.add("Project Code", g4)
+            dobj.metadata.add("Project Code", g5)
             if (veryverbose): print(dobj.metadata.items())
 
 
     if (len(subcollections) > 0):
         for subc in subcollections:
 
-            subc.metadata.add("project_uuid", projectUUID)
+            subc.metadata.add("Project Code", g1)
+            subc.metadata.add("Project Code", g2)
+            subc.metadata.add("Project Code", g3)
+            subc.metadata.add("Project Code", g4)
+            subc.metadata.add("Project Code", g5)
             if (veryverbose): print(subc.metadata.items())
 
